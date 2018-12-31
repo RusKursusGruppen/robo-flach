e75a0bc759f34b66e86da385f37e562a78c5e371
Benjamin <benjamin@rotendahl.dk>
2018-12-31 17:20:35 +0100

    Working lunches to frame

--- src/main.py
+++ src/main.py
@@ -5,34 +5,14 @@ from tqdm import tqdm
 import time
 
 gbot = Gbot()
-fbot = FbBot('<email>', '<password>')
-
-def fillNamesAndDates(lunch, row_nr):
-  gbot.sheet.update_cell(row_nr, 1, lunch['date'])
-  for name in lunch['reactions']:
-    gbot.sheet.update_cell(row_nr, nameToCol(name), 1)
+fbot = FbBot('email', 'pass')
 
 
-def nameToCol(target_name):
-  for name, col in gbot.columns:
-    if name == target_name:
-      return col
+
+lunches = gbot.lunches_to_table(
+  fbot.getLunches(),
+  [name for name, _ in fbot.names]
+)
 
 
-def get_empty_row():
-  row_nr = 1
-  while gbot.sheet.cell(row_nr, 1).value != '':
-    row_nr += 1
-    return row_nr
-
-
-def input_lunches():
-  gbot.fillNames([name for name, id in fbot.names])
-  row_nr = get_empty_row()
-  lunches = fbot._getLunches()
-  for lunch in tqdm(lunches):
-    fillRow(lunch, row_nr)
-    time.sleep(1)
-    row_nr += 1
-
-input_lunches()
+lunches.columns
