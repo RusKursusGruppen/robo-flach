0b7f32d4b41ef0d7cf41b5ac27581aa10a1aedd9
Benjamin <benjamin@rotendahl.dk>
2018-12-31 17:10:39 +0100

    Lunches to table implemented

--- src/googleBot.py
+++ src/googleBot.py
@@ -1,6 +1,6 @@
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials as SC
-
+import pandas as pd
 
 class Gbot():
   def __init__(self):
@@ -19,3 +19,27 @@ class Gbot():
     for i in range(2, len(names) + 1):
         self.sheet.update_cell(1, i,  names[i - 2] )
         self.columns.append((names[i - 2], i))
+
+  def lunches_to_table(lunches, names):
+    weeks = []
+    dates = []
+    for lunch in lunches:
+      year, week_nr, _ = lunch['date'].isocalendar()
+      week = str(year) + '-' + str(week_nr)
+      if week not in dates:
+        row = {'date' : week}
+        for name in names:
+          row[name] = 0
+        dates.append(week)
+        weeks.append(row)
+
+    weeks = pd.DataFrame(weeks)
+    weeks = weeks[['date'] +list(weeks.columns[:-1])]
+
+    for lunch in lunches:
+      year, week_nr, _ = lunch['date'].isocalendar()
+      week_row = weeks[weeks['date'] == str(year) + '-' + str(week_nr)]
+      for name in lunch['reactions']:
+        week_row[name] = week_row[name] + 1
+      weeks[weeks['date'] == str(year) + '-' + str(week_nr)] = week_row
+    return weeks
